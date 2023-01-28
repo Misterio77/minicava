c24681fe7c91548e0fb4f55a1882b0145c48d097
Gabriel Fontes <hi@m7.rs>
2023-01-28 17:14:33 -0300

    make number of bars configurable through arg

--- minicava.sh
+++ minicava.sh
@@ -2,9 +2,15 @@
 
 dict="s/;//g;s/0/▁/g;s/1/▂/g;s/2/▃/g;s/3/▄/g;s/4/▅/g;s/5/▆/g;s/6/▇/g;s/7/█/g;"
 
+if [ ! -z "$1" ]; then
+    bars="$1"
+else
+    bars=7
+fi
+
 config="
 [general]
-bars=7
+bars=$bars
 [output]
 method=raw
 data_format=ascii
